9a1fb55382a1ef5174859b941d4ce83bdcf71d4f
Maksim Yudzeshka <yudzeshka@gmail.com>
2021-11-11 08:57:20 +0300

    Topic-1 Task 9 done

---
+++ JS/Topic-1/Task9.js
@@ -0,0 +1,8 @@
+// Write a function min(a,b) which returns the least of two numbers a and b:
+// min(2, 5) == 2
+// min(3, -1) == -1
+// min(1, 1) == 1
+
+function min(a, b) {
+   return (a > b ?  b : a);
+}
