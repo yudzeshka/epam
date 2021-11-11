4b2806a9a84b15624ecc4098d083f92d28bdf086
Maksim Yudzeshka <yudzeshka@gmail.com>
2021-11-11 10:14:46 +0300

    Topic-1 Task 8 done

---
+++ JS/Topic-1/Task8.js
@@ -0,0 +1,8 @@
+// Write a loop which prompts for a number greater than 100. 
+// If the visitor enters another number – ask them to input again.
+// The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+
+let num;
+do {
+    num = prompt('Enter number greater than 100')
+} while (num <= 100 && num )
