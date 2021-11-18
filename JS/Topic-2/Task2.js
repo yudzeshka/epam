9d8f66428a478480acf7d50a424a0d82f2b34e00
Maksim Yudzeshka <yudzeshka@gmail.com>
2021-11-18 12:08:46 +0300

    Create Task2.js

---
+++ JS/Topic-2/Task2.js
@@ -0,0 +1,13 @@
+// Write a sum() function which will work properly when invoked using syntax below:
+// sum(2,3); // Outputs 5
+// sum(2)(3); // Outputs 5
+// sum(1)(2)(3)(4); // Outputs 10
+
+
+// let numbers = (1)(2)(3)(4);
+// console.log(RegExp(/\d+/)(numbers))
+// function sum(numbers) {
+//     numbers.split('()')
+//     console.log(numbers)
+// }
+// sum((2)(3))
